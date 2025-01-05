9895e8273686c108b0e36589355434eccf0c810c
ChunHou20c <chunhouthatbornin20c@gmail.com>
2025-01-05 23:31:15 +0800

    fix small bug

--- windows/status_bar/scripts/eww_toggle_window.sh
+++ windows/status_bar/scripts/eww_toggle_window.sh
@@ -3,23 +3,23 @@
 calendar() {
 
   CALENDER_REV=$(eww get calendar_rev)
-  if [ "$CALENDER_REV" = "true" ]; then 
+  if [ "$CALENDER_REV" = "false" ]; then 
     eww open calendar
-    eww update calendar_rev=false
+    eww update calendar_rev=true
   else
     eww close calendar
-    eww update calendar_rev=true
+    eww update calendar_rev=false
   fi
 }
 
 volume() {
   AUDIO_REV=$(eww get audioctl_reveal)
-  if [ "$AUDIO_REV" = "true" ]; then 
+  if [ "$AUDIO_REV" = "false" ]; then 
     eww open audio_ctl
-    eww update audioctl_reveal=false
+    eww update audioctl_reveal=true
   else
     eww close audio_ctl
-    eww update audioctl_reveal=true
+    eww update audioctl_reveal=false
   fi
 }
 
